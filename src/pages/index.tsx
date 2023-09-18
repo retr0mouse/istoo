2c5070212bb79b7f881b88a4cbe1562db909a728
Daniil <daniil.sharin667@gmail.com>
2023-09-18 20:22:32 +0300

    home page design first iteration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,38 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
+import { Koulen, Alegreya } from 'next/font/google'
+import TablesImage from '../images/wooden-tables-leaves.jpg';
+import Image from "next/image";
+
+const koulen = Koulen({
+  subsets: ['latin'],
+  weight: "400"
+});
+
+const alegreya = Alegreya({
+  subsets: ['latin'],
+  weight: '400'
+});
 
 export default function Home() {
-
   return (
     <>
       <Head>
         <title>Istoo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-100">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-slate-700 sm:text-[5rem]">
-            Istoo, palun!
-          </h1>
-        </div>
-        <Link href={"/create-plan"}><button className="mb-5 h-12 w-36 bg-blue-400 text-2xl text-slate-100 font-semibold ">Create</button></Link>
-        <div className="flex flex-col gap-2">
-          {
-            [1, 2, 3, 4, 5].map((firstKeys: number) => {
-              return (
-                <div className="flex gap-2">
-                  {[5, 4, 3, 2, 1].map((secondKeys) => {
-                    const [pressed, setPressed] = useState(false);
-                    return (
-                      <button onClick={() => pressed ? setPressed(false) : setPressed(true)}><div className={`h-12 w-12 ${pressed ? "bg-red-500 hover:bg-red-400" :"bg-green-300 hover:bg-green-200"} rounded-md p-2 border border-slate-400`} key={firstKeys + secondKeys}></div></button>
-                    );
-                  })}
-                </div>
-              );
-            })
-          }
+      <header className="bg-header-green flex items-start w-full h-12 p-2">
+        <h1 className={`${koulen.className} self-center uppercase text-sm font-extrabold tracking-tight text-slate-800 sm:text-2xl`}>
+          istoo.
+        </h1>
+      </header>
+      <main className="flex min-h-screen justify-between items-center  bg-slate-100 mt-0">
+        <Image className={'w-1/2'} src={TablesImage} alt={"Picture of wooden tables and green leaves hanging from a ceiling"}></Image>
+        <div className={'w-1/2 flex flex-col justify-center p-24 text-start'}>
+          <h1 className={`${alegreya.className} text-3xl font-extrabold`}>Book a table easily</h1>
+          <h2 className={`my-8`}>Say goodbye to waiting times with our restaurant seating app. Reserve your spot hassle-free.</h2>
+          <button className={`bg-button-green w-24 px-2 py-1 rounded-md`}><span className="text-white">Explore</span></button>
         </div>
       </main>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-green': '#A2D29E',
+        'button-green': '#2B762A'
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
