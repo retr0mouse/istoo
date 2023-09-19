4ced35acac37fa73bb072480e1c0358d01a57982
Daniil <daniil.sharin667@gmail.com>
2023-09-19 20:34:21 +0300

    second part of about page init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { useState } from "react";
 import { Koulen, Alegreya } from 'next/font/google'
 import TablesImage from '../images/wooden-tables-leaves.jpg';
+import WomanPhoneImage from '../images/woman-phone.jpg';
+import ReservedImage from '../images/reserved-table.jpg';
+import WoodenTables from '../images/tables.jpg';
 import Image from "next/image";
 
 const koulen = Koulen({
@@ -35,6 +38,28 @@ export default function Home() {
           <button className={`bg-button-green w-24 px-2 py-1 rounded-md`}><span className="text-white">Explore</span></button>
         </div>
       </main>
+      <div className="flex flex-col justify-center items-center w-full bg-gradient-to-r from-green-900 to-gray-600">
+        <div className="w-4/5">
+          <h1 className={`${alegreya.className} text-stone-200 text-4xl font-medium`}>Our Features</h1>
+          <div className="flex flex-col md:flex-row justify-center gap-12 py-12">
+            <div>
+              <Image className={'w-64 h-64 object-cover rounded-sm'} src={WomanPhoneImage} alt="Picture of a woman looking at her phone" />
+              <h1>Table reservation</h1>
+              <p>Effortlessly book a table for any occasion in just a few clicks. </p>
+            </div>
+            <div>
+              <Image className={'w-64 h-64 object-cover rounded-sm'} src={WoodenTables} alt="A picture of a wooden tables" />
+              <h1>Waitlist Management</h1>
+              <p>Manage waitlists dynamically and optimize customer seating experience.</p>
+            </div>
+            <div>
+              <Image className={'w-64 h-64 object-cover rounded-sm'} src={ReservedImage} alt="A picture of a reserved table" />
+              <h1>Real-Time Availability</h1>
+              <p>Check live availability and secure your preferred spot instantly. </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
